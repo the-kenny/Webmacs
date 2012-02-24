4ff151be3e7463b6bedeadb4459094f43f985b41
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-02-24 02:18:06 +0100

    test for base64-encoded data

--- test/webmacs/test/core.clj
+++ test/webmacs/test/core.clj
@@ -2,20 +2,23 @@
   (:use webmacs.core
         midje.sweet))
 
+(def encoded "RlVCQVI=")
+(def decoded "FUBAR")
+
 (facts
-  (parse-message '(insert 0 2 "aa")) => {:type :insert
+  (parse-message '(insert 0 2 encoded)) => {:type :insert
                                          :start 0
                                          :end 2
-                                         :data "aa"}
-  (parse-message '(replace 0 5 "aa")) => {:type :replace
+                                         :data decoded}
+  (parse-message '(replace 0 5 encoded)) => {:type :replace
                                           :start 0
                                           :end 5
-                                          :data "aa"}
+                                          :data decoded}
   (parse-message '(delete 0 5)) => {:type :delete
                                     :start 0
                                     :end 5}
-  (parse-message '(buffer-data 5 "fubar.org" "RlVCQVI="))
+  (parse-message '(buffer-data 5 "fubar.org" encoded))
   => {:type :buffer-data
       :name "fubar.org"
-      :length 5
-      :data "FUBAR"})
+      :length (count decoded)
+      :data decoded})
