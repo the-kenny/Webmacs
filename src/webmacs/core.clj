fab6a546e561275af7345b14e7707dd83db1234d
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-02-24 02:53:58 +0100

    apply changes to buffers (makes a few problems

--- src/webmacs/core.clj
+++ src/webmacs/core.clj
@@ -1,6 +1,7 @@
 (ns webmacs.core
   (:use [server.socket :as ss]
-        [clojure.java.io :as io])
+        [clojure.java.io :as io]
+        [webmacs.buffer :as buffer])
   (:import [org.apache.commons.codec.binary Base64]))
 
 (defn parse-message [term]
@@ -10,12 +11,12 @@
         (insert replace) (let [[_ start end data] term]
                            (let [decoded (String. (Base64/decodeBase64 ^String data) "utf-8")]
                              {:type (keyword op)
-                                    :start start
-                                    :end end
-                                    :data decoded}))
+                              :start start
+                              :end end
+                              :data decoded}))
         delete (let [[_ start end] term] {:type :delete
-                        :start start
-                        :end end})
+                                          :start start
+                                          :end end})
         buffer-data (let [[_ length name data] term
                           decoded (String. (Base64/decodeBase64 ^String data) "utf-8")]
                       (assert (= (count decoded) length))
@@ -24,14 +25,23 @@
                        :length length
                        :data decoded})))))
 
-(defn emacs-connection-loop [input output]
-  (println (str "" (parse-message (read input))))
-  ;; Trampoline instead of recur allows re-evaluation of functions from the repl
-  (trampoline #'emacs-connection-loop input output))
+(defn emacs-connection-loop [buffer input output]
+  (let [request (parse-message (read input))
+        _ (println request)
+        newbuffer (case (:type request)
+                    :buffer-data (assoc buffer :filename (:name request)
+                                        :contents (:data request))
+                    :insert (buffer/insert-data buffer (:start request) (:data request))
+                    :replace (buffer/replace-region buffer (:start request) (:end request) (:data request))
+                    :delete (buffer/delete-region buffer (:start request) (:end request)))]
+    (println newbuffer)
 
-(def server-socket (ss/create-server 9881 (fn [is os]
-                                            (let [ird (java.io.PushbackReader. (io/reader is))
-                                                  owr (io/writer os)]
-                                              (emacs-connection-loop ird owr)))))
+    ;; Trampoline instead of recur allows re-evaluation of functions from the repl
+    (trampoline #'emacs-connection-loop newbuffer input output)))
 
-(ss/close-server server-socket)
+;; (def server-socket (ss/create-server 9881 (fn [is os]
+;;                                             (let [ird (java.io.PushbackReader. (io/reader is))
+;;                                                   owr (io/writer os)]
+;;                                               (emacs-connection-loop (buffer/make-buffer) ird owr)))))
+
+;; (ss/close-server server-socket)
