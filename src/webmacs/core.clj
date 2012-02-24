3e72b406f2f5d698cf2c494774e4f3ec97e681f6
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-02-24 18:48:16 +0100

    don't use maps for parsed changes

--- src/webmacs/core.clj
+++ src/webmacs/core.clj
@@ -10,39 +10,34 @@
       (case op
         (insert replace) (let [[_ start end data] term]
                            (let [decoded (String. (Base64/decodeBase64 ^String data) "utf-8")]
-                             {:type (keyword op)
-                              :start (dec start)
-                              :end (dec end)
-                              :data decoded}))
-        delete (let [[_ start end] term] {:type :delete
-                                          :start (dec start)
-                                          :end (dec end)})
+                             [(keyword op) (dec start) (dec end) decoded]))
+        delete (let [[_ start end] term] [:delete (dec start) (dec end)])
         buffer-data (let [[_ length name data] term
                           decoded (String. (Base64/decodeBase64 ^String data) "utf-8")]
-                      (assert (= (count decoded) length))
-                      {:type :buffer-data
-                       :name name
-                       :length length
-                       :data decoded})))))
+                      (assert (= (count decoded) length)) ;TODO: Better error checking
+                      [:buffer-data name length decoded])))))
 
 (def ^:dynamic *buffer* nil)
 
 (defn emacs-connection-loop [input output]
-  (let [request (parse-message (read input))
-        _ (println request)
-        newbuffer (case (:type request)
-                    :buffer-data (assoc *buffer*
-                                   :filename (:name request)
-                                   :contents (:data request))
-                    :insert (buffer/insert-data *buffer* (:start request) (:data request))
-                    :replace (buffer/replace-region *buffer* (:start request) (:end request) (:data request))
-                    :delete (buffer/delete-region *buffer* (:start request) (:end request)))]
-    (println newbuffer)
+  (try
+   (let [request (parse-message (read input))
+         _ (println request)
+         newbuffer (case (first request)
+                     :buffer-data (assoc *buffer*
+                                    :filename (:name request)
+                                    :contents (:data request))
+                     :insert (let [[_ start _ data] request] (buffer/insert-data *buffer* start data))
+                     :replace (let [[_ start end data] request] (buffer/replace-region *buffer* start end data))
+                     :delete (let [[_ start end] request] (buffer/delete-region *buffer* start end)))]
+     (println newbuffer)
 
-    (set! *buffer* newbuffer)
+     (set! *buffer* newbuffer)
 
-    ;; Trampoline instead of recur allows re-evaluation of functions from the repl
-    (trampoline #'emacs-connection-loop input output)))
+     ;; Trampoline instead of recur allows re-evaluation of functions from the repl
+     (trampoline #'emacs-connection-loop input output))
+   (catch java.net.SocketException e
+       (println "Got SocketException:" (.getMessage e) "Exiting."))))
 
 #_(def server-socket (ss/create-server 9881 (fn [is os]
                                               (let [ird (java.io.PushbackReader. (io/reader is))
--- test/webmacs/test/core.clj
+++ test/webmacs/test/core.clj
@@ -6,19 +6,7 @@
 (def decoded "FUBAR")
 
 (facts
-  (parse-message (list 'insert 0 2 encoded)) => {:type :insert
-                                                 :start 0
-                                                 :end 2
-                                                 :data decoded}
-  (parse-message (list 'replace 0 5 encoded)) => {:type :replace
-                                                  :start 0
-                                                  :end 5
-                                                  :data decoded}
-  (parse-message (list 'delete 0 5)) => {:type :delete
-                                         :start 0
-                                         :end 5}
-  (parse-message (list 'buffer-data 5 "fubar.org" encoded))
-  => {:type :buffer-data
-      :name "fubar.org"
-      :length (count decoded)
-      :data decoded})
+  (parse-message (list 'insert 1 3 encoded)) => [:insert 0 2 decoded]
+  (parse-message (list 'replace 1 6 encoded)) => [:replace 0 5 decoded]
+  (parse-message (list 'delete 1 6)) => [:delete 0 5]
+  (parse-message (list 'buffer-data 5 "fubar.org" encoded)) => [:buffer-data "fubar.org" (count decoded) decoded])
