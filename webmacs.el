bb82ac6734ca18c6b824b116d924d8df81216f22
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-02-25 15:55:04 +0100

    rework elisp mode. Now uses a minor mode to track which buffers get published.

--- webmacs.el
+++ webmacs.el
@@ -1,3 +1,21 @@
+(defvar webmacs-buffer-name "*webmacs*")
+(defvar webmacs-process-name "webmacs connection")
+
+(defvar webmacs-host "localhost")
+(defvar webmacs-port 9881)
+
+(defun webmacs-open-connection (host port)
+  (interactive (list (read-from-minibuffer "Host: " webmacs-host)
+                     (read-from-minibuffer "Port: " (format "%d" webmacs-port)
+                                           nil t)))
+  (open-network-stream webmacs-process-name "*webmacs*" host port))
+
+(defun webmacs-publish-buffer (buffer-name)
+  (interactive "b")
+  (let ((buffer (get-buffer buffer-name)))
+    ;; (add-to-list 'after-change-functions #'webmacs-after-change)
+    (process-send-string webmacs-buffer-name (format "%S" (webmacs-generate-buffer-data buffer)))))
+
 (defun webmacs-encode-string (string)
   (base64-encode-string (encode-coding-string string 'utf-8) 'no-newline))
 
@@ -11,18 +29,26 @@
   (with-current-buffer (or buffer (current-buffer))
     (list 'buffer-data (buffer-name buffer) (buffer-size) (webmacs-encode-string (buffer-string)))))
 
-(defvar webmacs-process-name "webmacs connection")
-
 (defun webmacs-after-change (start end length)
-  (when (equal (current-buffer) (get-buffer "footest"))
-    (process-send-string "*webmacs*" (format "%S" (webmacs-generate-change-query start end length)))))
+  (if (processp (get-buffer-process webmacs-buffer-name))
+    (process-send-string webmacs-buffer-name (format "%S" (webmacs-generate-change-query start end length)))
+    (message "No webmacs connection. Please open it using `webmacs-open-connection'")))
 
-(defun webmacs-publish-buffer (buffer-name)
-  (interactive "b")
-  (let ((buffer (get-buffer buffer-name)))
-   (open-network-stream webmacs-process-name "*webmacs*" "localhost" 9881)
-   (add-to-list 'after-change-functions #'webmacs-after-change)
-   (process-send-string "*webmacs*" (format "%S" (webmacs-generate-buffer-data buffer)))))
+(define-minor-mode webmacs-mode
+  ""
+  :init-value nil
+  :lighter " webmacs"
+  :group 'webmacs
+
+  (if webmacs-mode
+    (progn
+      (if (not (get-buffer-process webmacs-buffer-name))
+        (message "No webmacs connection. Please open one using `webmacs-open-connection'")
+        ;; (if (yes-or-no-p "No webmacs connection. Open one? ")
+        ;;     (call-interactively #'webmacs-open-connection))
+        (add-hook 'after-change-functions #'webmacs-after-change nil 'local)
+        (webmacs-publish-buffer (buffer-name))))
+    (remove-hook 'after-change-functions #'webmacs-after-change 'local)))
 
 ;; (open-network-stream webmacs-process-name "*webmacs*" "localhost" 9881)
 
