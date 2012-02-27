5e7dcbeffea50cc5f7941aec5adbdddafc95e2c1
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-02-27 16:29:56 +0100

    remove outdated unit tests

--- test/webmacs/test/publishers.clj
+++ test/webmacs/test/publishers.clj
@@ -23,24 +23,3 @@
   (fact
     (store-buffer! (make-buffer +name+ "abc"))
     (buffer-changed! [:insert +name+ 0 1 "x"]) => (make-buffer +name+ "xabc")))
-
-(let [cchan (permanent-channel)
-      change-atom (atom nil)
-      buf (make-buffer +name+ "abc")]
-  (reset-publishers!)
-  (receive-all cchan #(reset! change-atom %))
-
-  ;; First, create a buffer
-  (facts
-    (buffer-changed! buf [:buffer-data (:filename buf) (count (:contents buf)) (:contents buf)])
-    @buffers => (contains {+name+ any}))
-
-  (fact
-    (add-client! +name+ cchan)
-    @change-atom => [:buffer-data +name+ 3 "abc"])
-
-  (fact
-    (buffer-changed! buf [:replace +name+ 0 1 "b"])
-    @change-atom => [:replace +name+ 0 1 "b"])
-
-  (future-fact "remove-client!"))
