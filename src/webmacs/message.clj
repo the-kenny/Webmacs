b01f7ee9bb2928b19e5cb9165f23e6f5169f605e
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-03-05 01:53:00 +0100

    use a multimethod for webmacs.message/parse

--- src/webmacs/message.clj
+++ src/webmacs/message.clj
@@ -1,17 +1,21 @@
 (ns webmacs.message
   (:import [org.apache.commons.codec.binary Base64]))
 
-;;; TODO: Use a multimethod
-(defn parse [term]
-  (when (and (sequential? term) (>= (count term) 3))
-    (let [op (first term)]
-      (case op
-        ;; TODO: :insert should only contain `at' and not `start' and `end'
-        (insert replace) (let [[_ buffer start end data] term]
-                           (let [decoded (String. (Base64/decodeBase64 ^String data) "utf-8")]
-                             [(keyword op) buffer (dec start) (dec end) decoded]))
-        delete (let [[_ buffer start end] term] [:delete buffer (dec start) (dec end)])
-        buffer-data (let [[_ buffer length data] term
-                          decoded (String. (Base64/decodeBase64 ^String data) "utf-8")]
-                      (assert (= (count decoded) length)) ;TODO: Better error checking
-                      [:buffer-data buffer length decoded])))))
+(defmulti parse first)
+
+;; TODO: :insert should only contain `at' and not `start' and `end'
+(defmethod parse 'insert [[op buffer start end data]]
+  (let [decoded (String. (Base64/decodeBase64 ^String data) "utf-8")]
+    [(keyword op) buffer (dec start) (dec end) decoded]))
+
+(defmethod parse 'replace [[op buffer start end data]]
+  (let [decoded (String. (Base64/decodeBase64 ^String data) "utf-8")]
+    [(keyword op) buffer (dec start) (dec end) decoded]))
+
+(defmethod parse 'delete [[_ buffer start end]]
+  [:delete buffer (dec start) (dec end)])
+
+(defmethod parse 'buffer-data [[_ buffer length data]]
+  (let [decoded (String. (Base64/decodeBase64 ^String data) "utf-8")]
+    (assert (= (count decoded) length)) ;TODO: Better error checking
+    [:buffer-data buffer length decoded]))
