6cf5e1a0cb3be9a8dbae6843a48f572764ee09cb
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-03-07 18:43:10 +0100

    update single buffer lines.

--- src-cljs/webmacs/display.cljs
+++ src-cljs/webmacs/display.cljs
@@ -7,26 +7,47 @@
 (defn ^:private make-line-span [n]
   (dom/element :span {:id (str "L" n)} (str n)))
 
+(defn ^:private append-newline [parent elem]
+  (dom/append parent elem "\n"))
+
+(defn ^:private remove-element-and-newline [elem]
+  (let [sibling (.-nextSibling elem)]
+   (gdom/removeNode elem)
+   (gdom/removeNode sibling)))
+
 (defn make-line-count [n]
   (let [pre (dom/element :pre {:id "line-numbers"})]
     (doseq [n (range 1 (inc n))]
-     (dom/append pre (make-line-span n) "\n"))
-   pre))
+      (append-newline pre (make-line-span n)))
+    pre))
 
 (defn update-line-count [parent n]
   (let [old-n (count (garray/toArray (gdom/getChildren parent)))]
     (cond
       (< old-n n) (doseq [i (range old-n n)]
-                    (dom/append parent (make-line-span i) "\n"))
+                    (append-newline parent (make-line-span i)))
       (> old-n n) (doseq [i (range n old-n)]
-                    (let [el (dom/get-element (str "L" i))
-                          sibling (.-nextSibling el)]
-                     (gdom/removeNode el)
-                     (gdom/removeNode sibling))))))
+                    (remove-element-and-newline (dom/get-element (str "L" i)))))))
 
 (defn make-buffer-contents [lines]
   (let [pre (dom/element :pre {:id "buffer-contents"})]
     (doseq [line lines]
-     (dom/append pre (dom/element :span line))
-     (dom/append pre "\n"))
-   pre))
+      (append-newline pre (dom/element :span line)))
+    pre))
+
+(defn update-buffer-contents [parent lines]
+  (let [new-n (count lines)
+        els (garray/toArray (gdom/getChildren parent))
+        old-n (count els)]
+    (cond
+      (< old-n new-n) (doseq [i (range old-n new-n)]
+                        (append-newline parent (dom/element :span (get lines i)))g)
+      (> old-n new-n) (doseq [i (range new-n old-n)]
+                        (remove-element-and-newline (get els i))))
+
+    (let [new-childs (garray/toArray (gdom/getChildren parent))]
+     (doseq [n (range 0 new-n),
+             :let [el (get new-childs n),
+                   text (get lines n)]]
+       (when (not= (dom/get-value el) text)
+         (dom/set-text el text))))))
