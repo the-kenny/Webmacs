f60d2530ef191baea6ea40542f20a6dcf0b5b78b
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-03-08 15:03:51 +0100

    nicer mode-line (scrolling percentage, buffer size)

--- src-cljs/webmacs/core.cljs
+++ src-cljs/webmacs/core.cljs
@@ -3,7 +3,10 @@
             [webmacs.display :as display]
             [cljs.reader :as reader]
             [clojure.browser.dom :as dom]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [clojure.browser.event :as event]
+            [goog.dom :as gdom]
+            [goog.events :as gevents]))
 
 (def *socket* nil)
 (def *buffer* nil)
@@ -13,8 +16,32 @@
   (binding [*print-meta* true]
     (.send *socket* (pr-str data))))
 
+(def ^:private +conversion+ 1024)
+
 (defn make-modeline [buffer]
-  (str "Buffer: " (:name buffer) " (" (:mode buffer) ")"))
+  (let [name (:name buffer)
+        mode (:mode buffer)
+        size (.-length (or (:contents buffer) ""))
+        size-str (cond
+                   (< +conversion+ size) (str (Math/floor (/ size +conversion+)) "k")
+                   (< (* +conversion+ +conversion+) size) (str (Math/floor (/ size (* +conversion+ +conversion+))) "M"))
+        pos (Math/floor (* 100 (/ (.-y (gdom/getDocumentScroll)) (gdom/getDocumentHeight))))
+        pos-str (cond
+                  (= 0 (- (gdom/getDocumentHeight) (+ (.-y (gdom/getDocumentScroll))
+                                                       (.-height (gdom/getViewportSize)))))
+                  "Bot"
+
+                  (pos? pos) (str pos "%")
+                  true "Top")]
+    (str "Buffer: " name "\t" pos-str " of " size-str "\t(" mode ")")))
+
+(defn update-modeline [parent buffer]
+  (dom/set-text parent (make-modeline buffer)))
+
+;;; NOTE: Why does't event/listen work?
+(gevents/listen (gdom/getWindow)
+                gevents/EventType.SCROLL
+                #(update-modeline (dom/get-element :mode-line) *buffer*))
 
 ;;; Connnection Stuff
 (defn handle-socket-message [socket-event]
@@ -22,7 +49,7 @@
     (set! *buffer* (buffer/apply-modification *buffer* obj))
     (set! *buffer-content-lines* (string/split-lines (:contents *buffer*)))
 
-    (dom/set-text (dom/get-element :mode-line) (make-modeline *buffer*))
+    (update-modeline (dom/get-element :mode-line) *buffer*)
 
     (display/update-line-count (dom/get-element :line-numbers) (count *buffer-content-lines*))
     (display/update-buffer-contents (dom/get-element :buffer-contents) *buffer-content-lines*)))
