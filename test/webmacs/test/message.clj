ead095457c173d1f61da0da9a156287641a228ca
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-03-14 15:51:45 +0100

    Fix tests for :buffer-data (the request was modified to include the major-mode)

--- test/webmacs/test/message.clj
+++ test/webmacs/test/message.clj
@@ -6,9 +6,10 @@
 (def decoded "FUBAR")
 
 (def buffer "fubar.org")
+(def mode "org-mode")
 
 (facts
   (parse (list 'insert buffer 1 encoded)) => [:insert buffer 0 decoded]
   (parse (list 'replace buffer 1 6 encoded)) => [:replace buffer 0 5 decoded]
   (parse (list 'delete buffer 1 6)) => [:delete buffer 0 5]
-  (parse (list 'buffer-data "fubar.org" 5 encoded)) => [:buffer-data buffer (count decoded) decoded])
+  (parse (list 'buffer-data buffer mode 5 encoded)) => [:buffer-data buffer mode (count decoded) decoded])
